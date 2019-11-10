9d2394450f76606e3a0c74cb4e5462871dad6408
Blythe Pollard <blytheepollard@gmail.com>
2019-11-10 17:43:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -99,10 +99,11 @@ class Student
     sql = <<-SQL 
      SELECT *
       FROM students
-      WHERE grade = ?
+      WHERE grade = 10
+      LIMIT ?
     SQL
  
-     DB[:conn].execute(sql).map do |row|
+     DB[:conn].execute(sql, number).map do |row|
       self.new_from_db(row)
     end
   end 
