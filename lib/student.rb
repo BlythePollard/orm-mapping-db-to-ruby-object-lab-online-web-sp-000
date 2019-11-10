97aa7926ac4d49b080d6cbaaa7ce43c21d4ef7cc
Blythe Pollard <blytheepollard@gmail.com>
2019-11-10 17:40:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,9 +66,33 @@ class Student
       WHERE grade = 9
     SQL
  
-     DB[:conn].execute(sql, grade).map do |row|
+     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
+  end
+  
+  def self.students_below_12th_grade
+   sql = <<-SQL 
+     SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+ 
+     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.first_student_in_grade_10
+   sql = <<-SQL 
+     SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+ 
+     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
 end
