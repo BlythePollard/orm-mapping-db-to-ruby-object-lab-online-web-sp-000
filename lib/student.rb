0101788c716dad700c4d05613f190335ea90d501
Blythe Pollard <blytheepollard@gmail.com>
2019-11-10 17:43:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,8 +95,17 @@ class Student
     end.first
   end
   
-  def self.first_X_students_in_grade_10
-    
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL 
+     SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+ 
+     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end 
   end
   
   def self.all_students_in_grade_X(grade)
